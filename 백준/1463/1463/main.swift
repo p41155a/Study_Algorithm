0a9da8815e8bf625c02b62befec0b896af2c1733
p41155a <50395024+p41155a@users.noreply.github.com>
2020-11-05 17:44:10 +0900

    Add files via upload

---
+++ 백준/1463/1463/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  1463
+//
+//  Created by Yoojin Park on 2020/11/05.
+//
+
+import Foundation
+
+var num = Int(readLine()!)!
+var Dp = [Int](repeating: 0, count: 100001)
+
+for i in 2..<num+1 {
+    Dp[i] = Dp[i-1] + 1
+    if i%2 == 0 {
+        Dp[i] = min(Dp[i], Dp[i/2] + 1)
+    }
+    if i%3 == 0 {
+        Dp[i] = min(Dp[i], Dp[i/3] + 1)
+    }
+    
+}
+
+print(Dp[num])
